a5d61eb5c41663e419a379bfc258bfaafb956eb7
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2026-01-15 17:40:06 +0200

    Added checking of leftover file in case it was uploaded from another pod

--- internal/backup/uploader.go
+++ internal/backup/uploader.go
@@ -2,22 +2,24 @@ package backup
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 // Uploader handles uploading files to S3.
 type Uploader struct {
-	client manager.UploadAPIClient
+	client *s3.Client
 	bucket string
 }
 
 // NewUploader creates a new Uploader.
-func NewUploader(client manager.UploadAPIClient, bucket string) *Uploader {
+func NewUploader(client *s3.Client, bucket string) *Uploader {
 	return &Uploader{
 		client: client,
 		bucket: bucket,
@@ -47,4 +49,20 @@ func (u *Uploader) Upload(ctx context.Context, localPath, key string) (err error
 	}
 
 	return nil
+}
+
+// FileExists checks if a file exists in S3.
+func (u *Uploader) FileExists(ctx context.Context, key string) (bool, error) {
+	_, err := u.client.HeadObject(ctx, &s3.HeadObjectInput{
+		Bucket: aws.String(u.bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		var notFound *types.NotFound
+		if errors.As(err, &notFound) {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to check if file exists s3://%s/%s: %w", u.bucket, key, err)
+	}
+	return true, nil
 }
